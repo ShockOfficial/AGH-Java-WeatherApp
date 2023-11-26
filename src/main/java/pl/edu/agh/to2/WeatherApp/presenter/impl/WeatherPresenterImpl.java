1c53459ea150c5746b27a09d3a5d0f3cda440809
Marcin <mrcnsaja@gmail.com>
2023-11-26 11:43:43 +0100

    Add display of key information about current weather

--- src/main/java/pl/edu/agh/to2/WeatherApp/presenter/impl/WeatherPresenterImpl.java
+++ src/main/java/pl/edu/agh/to2/WeatherApp/presenter/impl/WeatherPresenterImpl.java
@@ -20,7 +20,7 @@ public class WeatherPresenterImpl implements WeatherPresenter {
     public void getWeatherByCity(String city) {
         try {
             WeatherData weatherData = model.getWeatherDataByCity(city);
-            view.updateWeatherDisplay(weatherData.toString());
+            updateWeatherDisplay(weatherData);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -30,9 +30,49 @@ public class WeatherPresenterImpl implements WeatherPresenter {
     public void getWeatherByCoordinates(String lat, String lon) {
         try {
             WeatherData weatherData = model.getWeatherDataByCoordinates(lon, lat);
-            view.updateWeatherDisplay(weatherData.toString());
+            updateWeatherDisplay(weatherData);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void updateWeatherDisplay(WeatherData weatherData) {
+        if (weatherData.getSys() != null) {
+            view.setWeatherOutputInformer("Weather in " + weatherData.getName() + " (" + weatherData.getSys().getCountry() + "): " + weatherData.getWeather().get(0).getMain() + "\n");
+
+            view.setCloudinessValue(Integer.toString(weatherData.getClouds().getAll()));
+            view.setPressureValue(Integer.toString(weatherData.getMain().getPressure()));
+            view.setHumidityValue(Integer.toString(weatherData.getMain().getHumidity()));
+            view.setWindValue(Double.toString(round(weatherData.getWind().getSpeed(), 2)));
+
+            if (weatherData.getRain() != null) {
+                view.setRainValue(Double.toString(weatherData.getRain().get1h()));
+            } else {
+                view.setRainValue("Unknown");
+            }
+
+            if (weatherData.getSnow() != null) {
+                view.setSnowValue(Double.toString(weatherData.getSnow().get1h()));
+            } else {
+                view.setSnowValue("Unknown");
+            }
+
+            view.setTemperatureValue(Double.toString(round(weatherData.getMain().getTemp(), 2)));
+            view.setSensedTemperatureValue(Double.toString(round(weatherData.getMain().getFeels_like(), 2)));
+            view.setMinimumTemperatureValue(Double.toString(round(weatherData.getMain().getTemp_min(), 2)));
+            view.setMaximumTemperatureValue(Double.toString(round(weatherData.getMain().getTemp_max(), 2)));
+
+            if (!view.isWeatherDisplaying()) {
+                view.setWeatherDisplaying(true);
+            }
+        } else {
+            view.setWeatherError("City not found");
+            view.setWeatherDisplaying(false);
+        }
+    }
+
+    private double round(double value, int places) {
+        double scale = Math.pow(10, places);
+        return Math.round(value * scale) / scale;
     }
 }
--- src/main/java/pl/edu/agh/to2/WeatherApp/view/WeatherView.java
+++ src/main/java/pl/edu/agh/to2/WeatherApp/view/WeatherView.java
@@ -3,20 +3,42 @@ package pl.edu.agh.to2.WeatherApp.view;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import pl.edu.agh.to2.WeatherApp.presenter.WeatherPresenter;
 
 public class WeatherView {
     @FXML
     private TextField cityInput;
-
     @FXML
     private TextField latitudeInput;
-
     @FXML
     private TextField longitudeInput;
-
     @FXML
-    private Label weatherOutput;
+    public Label weatherError;
+    @FXML
+    private VBox weatherInfo;
+    @FXML
+    private Label weatherOutputInformer;
+    @FXML
+    private Label cloudinessValue;
+    @FXML
+    private Label humidityValue;
+    @FXML
+    private Label pressureValue;
+    @FXML
+    private Label windValue;
+    @FXML
+    private Label rainValue;
+    @FXML
+    public Label snowValue;
+    @FXML
+    private Label temperatureValue;
+    @FXML
+    private Label minimumTemperatureValue;
+    @FXML
+    private Label sensedTemperatureValue;
+    @FXML
+    private Label maximumTemperatureValue;
 
     private WeatherPresenter presenter;
 
@@ -31,11 +53,64 @@ public class WeatherView {
         } else if (!latitudeInput.getText().isEmpty() && !longitudeInput.getText().isEmpty()) {
             presenter.getWeatherByCoordinates(latitudeInput.getText(), longitudeInput.getText());
         } else {
-            weatherOutput.setText("Please provide city name or coordinates");
+            setWeatherDisplaying(false);
+            weatherError.setText("Please provide city name or coordinates");
         }
     }
+    public void setWeatherError(String weatherError) {
+        this.weatherError.setText(weatherError);
+    }
 
-    public void updateWeatherDisplay(String weatherInfo) {
-        // TODO
+    public void setWeatherOutputInformer(String weatherOutput) {
+        this.weatherOutputInformer.setText(weatherOutput);
+    }
+
+    public boolean isWeatherDisplaying() {
+        return weatherInfo.isVisible();
+    }
+
+    public void setWeatherDisplaying(boolean weatherDisplaying) {
+        this.weatherInfo.setVisible(weatherDisplaying);
+        this.weatherError.setVisible(!weatherDisplaying);
+    }
+
+    public void setCloudinessValue(String cloudinessValue) {
+        this.cloudinessValue.setText(cloudinessValue);
+    }
+
+    public void setHumidityValue(String humidityValue) {
+        this.humidityValue.setText(humidityValue);
+    }
+
+    public void setPressureValue(String pressureValue) {
+        this.pressureValue.setText(pressureValue);
+    }
+
+    public void setWindValue(String windValue) {
+        this.windValue.setText(windValue);
+    }
+
+    public void setRainValue(String rainValue) {
+        this.rainValue.setText(rainValue);
+    }
+
+    public void setSnowValue(String snowValue) {
+        this.snowValue.setText(snowValue);
+    }
+
+    public void setTemperatureValue(String temperatureValue) {
+        this.temperatureValue.setText(temperatureValue);
+    }
+
+    public void setMinimumTemperatureValue(String minimumTemperatureValue) {
+        this.minimumTemperatureValue.setText(minimumTemperatureValue);
+    }
+
+    public void setSensedTemperatureValue(String sensedTemperatureValue) {
+        this.sensedTemperatureValue.setText(sensedTemperatureValue);
+    }
+
+    public void setMaximumTemperatureValue(String maximumTemperatureValue) {
+        this.maximumTemperatureValue.setText(maximumTemperatureValue);
     }
 }
