dec27f39e8e5e43bff5f93dff2df4810ec0c0adc
mczajor <mczajor@student.agh.edu.pl>
2023-12-03 20:52:29 +0100

    Added test for GUI and deleted useless field in App

--- src/main/java/pl/edu/agh/to2/WeatherApp/App.java
+++ src/main/java/pl/edu/agh/to2/WeatherApp/App.java
@@ -17,17 +17,11 @@ import java.io.IOException;
 
 public class App extends Application {
     private WeatherModel weatherModel;
-    private WeatherView weatherView;
 
     @Override
     public void init() {
         Injector injector = Guice.createInjector(new WeatherModule());
         weatherModel = injector.getInstance(WeatherModelImpl.class);
-        // in case of MVP dependency injection is hard/impossible because dependencies are cyclic
-        // presenter depends on view and view on presenter
-        weatherView = new WeatherView();
-        WeatherPresenterImpl presenter = new WeatherPresenterImpl(weatherModel, weatherView);
-        weatherView.setPresenter(presenter);
     }
 
     @Override
