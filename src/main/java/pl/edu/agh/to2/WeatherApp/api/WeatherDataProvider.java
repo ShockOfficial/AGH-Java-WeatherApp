f4df5479e7ced72748225cd70196b81b81fd0138
mczajor <mczajor@student.agh.edu.pl>
2023-12-09 12:21:36 +0100

    added air pollution API call

--- src/main/java/pl/edu/agh/to2/WeatherApp/api/WeatherDataProvider.java
+++ src/main/java/pl/edu/agh/to2/WeatherApp/api/WeatherDataProvider.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public class WeatherDataProvider {
     //API key has a limit of 100 calls a day if the limit is reached replace this one
     private static final String apiKey = "09fe2451db15d2b60a0a52041ee82126";
-    private static final String apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+    private static final String apiUrl = "https://api.openweathermap.org/data/2.5";
     private static final String apiKeyParamName = "appid";
     private static final String latitudeParamName = "lat";
     private static final String longitudeParamName = "lon";
@@ -20,13 +20,13 @@ public class WeatherDataProvider {
 
     //Getting weather using longitude and latitude
     public static String getWeather(String lon, String lat) throws IOException {
-        Response response = makeApiCall(Map.of(latitudeParamName, lat, longitudeParamName, lon));
+        Response response = makeApiCall(Map.of(latitudeParamName, lat, longitudeParamName, lon), "weather");
         return response.body().string();                            //returning the text in the body response
     }
 
     //This way is technically "deprecated" but works fine
     public static String getWeather(String city) throws IOException {
-        Response response = makeApiCall(Map.of(cityParamName, city));
+        Response response = makeApiCall(Map.of(cityParamName, city), "weather");
         return response.body().string();                            //returning the text in the body response
     }
 
@@ -34,8 +34,9 @@ public class WeatherDataProvider {
         return "https://openweathermap.org/img/wn/" + iconCode + "@4x.png";
     }
 
-    public static Response makeApiCall(Map<String, String> params) throws IOException {
+    public static Response makeApiCall(Map<String, String> params, String call_type) throws IOException {
         HttpUrl.Builder urlBuilder = HttpUrl.parse(apiUrl).newBuilder();
+        urlBuilder.addPathSegment(call_type);
         for (Map.Entry<String, String> entry : params.entrySet()) {
             urlBuilder.addQueryParameter(entry.getKey(), entry.getValue());
         }
@@ -44,5 +45,11 @@ public class WeatherDataProvider {
 
         Request request = new Request.Builder().url(url).build();
         return client.newCall(request).execute();                   //executing the request and getting HTTPOk response
+    }
+
+    //The API does not support air pollution calls with city name
+    public static void getAirPolution(String lon, String lat) throws IOException {
+        Response response = makeApiCall(Map.of(latitudeParamName, lat, longitudeParamName, lon), "air_pollution");
+        System.out.println(response.body().string());                           //returning the text in the body response
     }
 }
