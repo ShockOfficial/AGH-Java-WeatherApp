05e7b844089938b834c14437afe59cb7bfe15292
mczajor <mczajor@student.agh.edu.pl>
2023-12-09 15:02:55 +0100

    chagned seperate providers into one DataProvider

---
+++ src/main/java/pl/edu/agh/to2/WeatherApp/api/DataProvider.java
@@ -0,0 +1,64 @@
+package pl.edu.agh.to2.WeatherApp.api;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class DataProvider {
+    //API key has a limit of 100 calls a day if the limit is reached replace this one
+    private static final String apiKey = "09fe2451db15d2b60a0a52041ee82126";
+    private static final String url = "https://api.openweathermap.org";
+    private static final String apiKeyParamName = "appid";
+    private static final String cityParamName = "q";
+    private static final String limitParamName = "limit";
+    private static final String limit = "1";
+    private static final String latitudeParamName = "lat";
+    private static final String longitudeParamName = "lon";
+    private static final Map<String, String> apiUrls = Map.of(
+            "geo", "geo/1.0/direct",
+            "air", "data/2.5/air_pollution",
+            "weather", "data/2.5/weather" );
+    private static final OkHttpClient client = new OkHttpClient();
+
+    protected static Response makeApiCall(Map<String, String> params, String apiUrl) throws IOException {
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(url).newBuilder();
+        urlBuilder.addPathSegment(apiUrl);
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            urlBuilder.addQueryParameter(entry.getKey(), entry.getValue());
+        }
+        urlBuilder.addQueryParameter(apiKeyParamName, apiKey);      //api key is always required
+        String url = urlBuilder.build().toString();
+        System.out.println(url);
+
+        Request request = new Request.Builder().url(url).build();
+        return client.newCall(request).execute();                   //executing the request and getting HTTPOk response
+    }
+
+    public static String getWeather(String lon, String lat) throws IOException {
+        Response response = makeApiCall(Map.of(latitudeParamName, lat, longitudeParamName, lon), apiUrls.get("weather"));
+        return response.body().string();                            //returning the text in the body response
+    }
+
+    //This way is technically deprecated but works in a pinch, not recommended
+    public static String getWeather(String city) throws IOException {
+        Response response = makeApiCall(Map.of(cityParamName, city), apiUrls.get("weather"));
+        return response.body().string();                            //returning the text in the body response
+    }
+
+    public static String getCoords(String city) throws IOException {
+        Response response = makeApiCall(Map.of(cityParamName, city, limitParamName, limit), apiUrls.get("geo"));
+        return response.body().string()
+                .substring(1, response.body().string().length()-1);  //returning the text in the body response
+    }
+
+    public static String getAirPollution(String lon, String lat) throws IOException {
+        Response response = makeApiCall(Map.of(latitudeParamName, lat, longitudeParamName, lon), apiUrls.get("air"));
+        return response.body().string();                          //returning the text in the body response
+    }
+
+
+}
