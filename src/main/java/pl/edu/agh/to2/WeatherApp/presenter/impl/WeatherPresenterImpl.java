091c0b4caa5902e73cb2eafc2d71f1684e849048
mczajor <mczajor@student.agh.edu.pl>
2023-12-10 11:26:59 +0100

    fixed calculcating perceived temp(equation requires temp in celcius)

--- src/main/java/pl/edu/agh/to2/WeatherApp/presenter/impl/WeatherPresenterImpl.java
+++ src/main/java/pl/edu/agh/to2/WeatherApp/presenter/impl/WeatherPresenterImpl.java
@@ -63,6 +63,6 @@ public class WeatherPresenterImpl implements WeatherPresenter {
 
     private double get_feels_like(WeatherData data){
         return TempCalculator.CalculatePerceivedTemp(
-                data.getMain().getTemp(), data.getWind().getSpeed());
+                data.getMain().getTemp(), data.getWind().getSpeed()) + 273; //+273 is temporary only so that the unit matches the rest of the temperatures, remove after proper conversion is done
     }
 }
--- src/main/java/pl/edu/agh/to2/WeatherApp/utils/TempCalculator.java
+++ src/main/java/pl/edu/agh/to2/WeatherApp/utils/TempCalculator.java
@@ -3,6 +3,7 @@ package pl.edu.agh.to2.WeatherApp.utils;
 public class TempCalculator {
     public static double CalculatePerceivedTemp(float temp, float wind_speed){
         double conv_speed = Math.pow(wind_speed, 0.16);
-        return 13.12 + 0.6215*temp - 11.37*conv_speed + 0.3965*temp*conv_speed;
+        double conv_temp = temp - 273;
+        return 13.12 + 0.6215*conv_temp - 11.37*conv_speed + 0.3965*conv_temp*conv_speed;
     }
 }
