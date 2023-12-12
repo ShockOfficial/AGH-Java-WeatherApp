e30cdc0400905e41b5509c53cc0c55f6ff538bef
Adrian Żerebiec <412306@student.agh.edu.pl>
2023-12-12 13:11:14 +0100

    Removed TODO

--- src/main/java/pl/edu/agh/to2/weather_app/presenter/WeatherPresenter.java
+++ src/main/java/pl/edu/agh/to2/weather_app/presenter/WeatherPresenter.java
@@ -3,6 +3,6 @@ package pl.edu.agh.to2.weather_app.presenter;
 public interface WeatherPresenter {
     void getWeatherByCity(String city);
     void getWeatherByCoordinates(String lat, String lon);
-    void getWeatherByCities(String cityA, String cityB); // TODO Test this
-    void getWeatherByCoordinates(String latA, String lonA, String latB, String lonB); // TODO Test this
+    void getWeatherByCities(String cityA, String cityB);
+    void getWeatherByCoordinates(String latA, String lonA, String latB, String lonB);
 }
